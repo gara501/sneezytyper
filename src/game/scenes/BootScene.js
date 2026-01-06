c08e2810cf3ce94e2a2f609b9713b7bb867e52e8
aramirez13 <andres.ramirez.-nd@disney.com>
2026-01-06 13:30:10 -0500

    Deploy

--- src/game/scenes/BootScene.js
+++ src/game/scenes/BootScene.js
@@ -6,23 +6,23 @@ export default class BootScene extends Phaser.Scene {
     }
 
     preload() {
-        this.load.audio('sneeze1', '/sounds/jul1.mp3')
-        this.load.audio('sneeze2', '/sounds/jul2.mp3')
-        this.load.audio('sneeze3', '/sounds/jul3.mp3')
-        this.load.audio('sneeze4', '/sounds/jul4.mp3')
-        this.load.audio('sneeze5', '/sounds/jul5.mp3')
-        this.load.audio('sneeze6', '/sounds/jul6.mp3')
-        this.load.audio('correct', '/sounds/correct.mp3')
-        this.load.audio('music', '/sounds/music.mp3')
+        this.load.audio('sneeze1', `${import.meta.env.BASE_URL}sounds/jul1.mp3`)
+        this.load.audio('sneeze2', `${import.meta.env.BASE_URL}sounds/jul2.mp3`)
+        this.load.audio('sneeze3', `${import.meta.env.BASE_URL}sounds/jul3.mp3`)
+        this.load.audio('sneeze4', `${import.meta.env.BASE_URL}sounds/jul4.mp3`)
+        this.load.audio('sneeze5', `${import.meta.env.BASE_URL}sounds/jul5.mp3`)
+        this.load.audio('sneeze6', `${import.meta.env.BASE_URL}sounds/jul6.mp3`)
+        this.load.audio('correct', `${import.meta.env.BASE_URL}sounds/correct.mp3`)
+        this.load.audio('music', `${import.meta.env.BASE_URL}sounds/music.mp3`)
 
-        this.load.image('bg-play', '/bg/room2.png')
-        this.load.image('bg-intro', '/bg/bg.png')
-        this.load.image('bg-gameover', '/bg/gameover.png')
-        this.load.image('bg-credits', '/bg/credits.png')
-        this.load.image('j-1', '/images/j1.png')
-        this.load.image('j-2', '/images/j2.png')
-        this.load.image('s-on', '/images/sound.png')
-        this.load.image('s-off', '/images/mute.png')
+        this.load.image('bg-play', `${import.meta.env.BASE_URL}bg/room2.png`)
+        this.load.image('bg-intro', `${import.meta.env.BASE_URL}bg/bg.png`)
+        this.load.image('bg-gameover', `${import.meta.env.BASE_URL}bg/gameover.png`)
+        this.load.image('bg-credits', `${import.meta.env.BASE_URL}bg/credits.png`)
+        this.load.image('j-1', `${import.meta.env.BASE_URL}images/j1.png`)
+        this.load.image('j-2', `${import.meta.env.BASE_URL}images/j2.png`)
+        this.load.image('s-on', `${import.meta.env.BASE_URL}images/sound.png`)
+        this.load.image('s-off', `${import.meta.env.BASE_URL}images/mute.png`)
 
     }
 
